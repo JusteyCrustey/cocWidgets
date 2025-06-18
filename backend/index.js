7fd23db8d8bfaf2f9761a820d3a0607a414e55c9
justeycrustey <bitjust01@gmail.com>
2025-06-18 12:45:59 -0400

    enaled CORS for all routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 //setup
 require('dotenv').config();
 const express = require('express');
+const cors    = require('cors');
 const rateLimit = require('express-rate-limit');
 const axios = require('axios');
 const app = express();
